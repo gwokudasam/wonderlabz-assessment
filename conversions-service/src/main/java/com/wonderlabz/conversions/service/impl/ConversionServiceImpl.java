60846998b4c5b0d30e7452e2602735ce9eb9ad62
Samuel Gwokuda <gwokudasam@gmail.com>
2021-01-24 14:26:56 +0200

    Update ConversionService

--- conversions-service/src/main/java/com/wonderlabz/conversions/service/impl/ConversionServiceImpl.java
+++ conversions-service/src/main/java/com/wonderlabz/conversions/service/impl/ConversionServiceImpl.java
@@ -1,8 +1,14 @@
 package com.wonderlabz.conversions.service.impl;
 
+import com.wonderlabz.conversions.dto.CelsiusRequestDTO;
+import com.wonderlabz.conversions.dto.KelvinTempRequestDTO;
+import com.wonderlabz.conversions.dto.KilometreRequestDTO;
+import com.wonderlabz.conversions.dto.MileRequestDTO;
 import com.wonderlabz.conversions.service.api.ConversionService;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
+
+import javax.validation.Valid;
 
 /**
  * <p>
@@ -18,4 +24,24 @@ import org.springframework.stereotype.Service;
 @Service
 @Log4j2
 public class ConversionServiceImpl implements ConversionService {
+
+    @Override
+    public double celsiusToKelvin(@Valid CelsiusRequestDTO celsiusRequestDTO) {
+        return 0;
+    }
+
+    @Override
+    public double kelvinToCelsius(@Valid KelvinTempRequestDTO kelvinTempRequestDTO) {
+        return 0;
+    }
+
+    @Override
+    public double milesToKilometres(@Valid MileRequestDTO mileRequestDTO) {
+        return 0;
+    }
+
+    @Override
+    public double kilometresToMiles(@Valid KilometreRequestDTO kilometreRequestDTO) {
+        return 0;
+    }
 }
