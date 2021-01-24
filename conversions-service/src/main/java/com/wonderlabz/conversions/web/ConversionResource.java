37dfb3fc1a133b12d9b21d0755a5c411d7c03a07
Samuel Gwokuda <gwokudasam@gmail.com>
2021-01-24 13:56:31 +0200

    Add ConversionResource.java

---
+++ conversions-service/src/main/java/com/wonderlabz/conversions/web/ConversionResource.java
@@ -0,0 +1,17 @@
+package com.wonderlabz.conversions.web;
+
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Class <code>ConversionResource</code> is the implementation of the main Conversion
+ * Endpoint API definition.
+ *
+ * @author Samuel Gwokuda <gwokudasam@gmail.com>
+ * @version v1.0
+ * @since v1.0
+ */
+@RestController
+@Log4j2
+public class ConversionResource {
+}
