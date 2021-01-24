0dafa5b0b939ad37c79a5a0bc2cae6350b0d4ea6
Samuel Gwokuda <gwokudasam@gmail.com>
2021-01-24 14:47:38 +0200

    Update ConversionResource Add project SCM in pom.xml

--- conversions-service/src/main/java/com/wonderlabz/conversions/web/ConversionResource.java
+++ conversions-service/src/main/java/com/wonderlabz/conversions/web/ConversionResource.java
@@ -1,8 +1,17 @@
 package com.wonderlabz.conversions.web;
 
+import com.wonderlabz.conversions.dto.CelsiusRequestDTO;
+import com.wonderlabz.conversions.dto.KelvinTempRequestDTO;
+import com.wonderlabz.conversions.dto.KilometreRequestDTO;
+import com.wonderlabz.conversions.dto.MileRequestDTO;
 import com.wonderlabz.conversions.endpoint.ConversionEndPoint;
+import com.wonderlabz.conversions.service.api.ConversionService;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
 
 /**
  * Class <code>ConversionResource</code> is the implementation of the main Conversion
@@ -10,10 +19,60 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author Samuel Gwokuda <gwokudasam@gmail.com>
  * @version v1.0
- * @since v1.0
  * @see ConversionEndPoint
+ * @since v1.0
  */
 @RestController
 @Log4j2
-public class ConversionResource {
+public class ConversionResource implements ConversionEndPoint {
+
+    /** Conversion service business logic interface. */
+    private final ConversionService conversionService;
+
+    /**
+     * Creates an instance of {@link ConversionResource}
+     * @param conversionService The business logic service interface,
+     *                                   dependency-injected by the spring container
+     * */
+    @Autowired
+    public ConversionResource(@Qualifier("conversionServiceImpl") final ConversionService conversionService) {
+        this.conversionService = conversionService;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public double celsiusToKelvin(@Valid final CelsiusRequestDTO celsiusRequestDTO) {
+        log.debug("REST request to convert {} centigrade to kelvin", celsiusRequestDTO);
+        return conversionService.celsiusToKelvin(celsiusRequestDTO);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public double kelvinToCelsius(@Valid final KelvinTempRequestDTO kelvinTempRequestDTO) {
+        log.debug("REST request to convert {} kelvins to centigrade", kelvinTempRequestDTO);
+        return conversionService.kelvinToCelsius(kelvinTempRequestDTO);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public double milesToKilometres(@Valid final MileRequestDTO mileRequestDTO) {
+        log.debug("REST request to convert {} miles to km", mileRequestDTO);
+        return conversionService.milesToKilometres(mileRequestDTO);
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public double kilometresToMiles(@Valid final KilometreRequestDTO kilometreRequestDTO) {
+        log.debug("REST request to convert {} km to miles", kilometreRequestDTO);
+        return conversionService.kilometresToMiles(kilometreRequestDTO);
+    }
+
 }
