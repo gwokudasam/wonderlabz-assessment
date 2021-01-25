5d3b3a6d7205e98f7db4889a13b3c04e2dd0cb52
Samuel Gwokuda <gwokudasam@gmail.com>
2021-01-25 16:07:11 +0200

    Add app comments Roulette file utility Add player data - players.dat

--- conversions-service/src/main/java/com/wonderlabz/conversions/ConversionsServiceApplication.java
+++ conversions-service/src/main/java/com/wonderlabz/conversions/ConversionsServiceApplication.java
@@ -3,6 +3,15 @@ package com.wonderlabz.conversions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * <p>
+ * Conversions Application
+ * </p>
+ *
+ * @author Samuel Gwokuda <gwokudasam@gmail.com>
+ * @version v1.0
+ * @since v1.0
+ */
 @SpringBootApplication
 public class ConversionsServiceApplication {
 
--- roulette-console/src/main/java/com/wonderlabz/roulette/RouletteConsoleApplication.java
+++ roulette-console/src/main/java/com/wonderlabz/roulette/RouletteConsoleApplication.java
@@ -2,12 +2,19 @@ package com.wonderlabz.roulette;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+/**
+ * <p>
+ * Console Roulette Application
+ * </p>
+ *
+ * @author Samuel Gwokuda <gwokudasam@gmail.com>
+ * @version v1.0
+ * @since v1.0
+ */
 @SpringBootApplication
 public class RouletteConsoleApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(RouletteConsoleApplication.class, args);
     }
-
 }
